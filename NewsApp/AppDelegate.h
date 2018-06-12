5f04c84397304d213ecdbaa5897dbf6b9d01a45f
chenyule <1069875706@qq.com>
2018-06-12 11:32:22 +0800

    Initial Commit

---
+++ NewsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsApp
+//
+//  Created by 陈玉乐 on 2018/6/12.
+//  Copyright © 2018年 陈玉乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsApp
+//
+//  Created by 陈玉乐 on 2018/6/12.
+//  Copyright © 2018年 陈玉乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
